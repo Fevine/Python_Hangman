684737a8e67e33a1dee67246be0ed6299ca14af9
Fevine <albinoni1423@gmail.com>
2023-09-24 11:45:31 +0400

    Adding Hangman.py

---
+++ Hangman.py
@@ -0,0 +1,47 @@
+from random import choice
+
+Animals = [
+    'fox', 'cat', 'chicken', 'dog', 'tiger', 'cheetah', 'leopard',
+    'crocodile', 'zebra', 'goat', 'sheep', 'rabbit', 'deer', 'elephant',
+    'rhino', 'hummingbird', 'wolf', 'camel', 'shark', 'peacock', 'lion', 'griffe',
+    'Whale', 'snake', 'panda', 'koala'
+]
+
+def update (a):
+    global Target, Hidden, Result
+    a = a.lower()
+    for i in range(int(len(Target))):
+        if a == Target[i]:
+            Hidden[i]=a
+        Result = ''.join(Hidden)
+    return Result
+
+print('Find the hidden animal')
+
+Target = choice(Animals)
+Hidden = list(len(Target)*'_')
+Result = ''.join(Hidden)
+Chances = 6
+Try = input('Try to find: ')
+
+while Chances > 0 and Result != Target:
+    if Try != Target:
+        if Try in Target:
+            print('You found a letter')
+            print(update(Try),'\n')
+            if Result == Target:
+                print('You won!')
+                break
+        else:
+            print(f'{Try} is not in the word\n')
+            print(Result,'\n')
+            Chances -=1
+            print(f'Remaining chances {Chances}')
+            if Chances == 0:
+                print('You lose!')
+                print(f'The animal was {Target}')
+                break
+        Try = input('Try to find: ')
+    else:
+        print('\nYou won!')
+        break
